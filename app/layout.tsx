9d21edc3314682e67faae772461fed0b8ba1862c
Madhav Bhardwaj <madhavbhardwaj16@gmail.com>
2024-08-15 17:37:12 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import "./globals.css"
+import { Inter as FontSans } from "next/font/google"
+import { Metadata } from "next"
+import { cn } from "@/lib/utils"
+import {dark} from "@clerk/themes"
+import { ClerkProvider } from "@clerk/nextjs"
+import Provider from "./Provider"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "Collaborative editor just for you",
+};
+
+export default function RootLayout({ children }: {children : React.ReactNode}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: '#3371FF',
+          fontSize: '16px',
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Provider>
+            {children}
+          </Provider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
